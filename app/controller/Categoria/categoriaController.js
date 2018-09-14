7c2fd70032e0010619f432571a83812d09818a72
rafael_alcindo <rafael_alcindo@hotmail.com>
2018-09-14 14:02:04 -0300

    categoria atualizar

--- app/controller/Categoria/categoriaController.js
+++ app/controller/Categoria/categoriaController.js
@@ -4,5 +4,33 @@ const categoriaModel = mongoose.model('Categoria',BlogCategoriaSchema);
 var categoria = new categoriaModel;
 
 module.exports.cadastroCategoria = (app, req, res) => {
+    categoria._id       = new mongoose.Types.ObjectId;
+    categoria.categoria = req.body.categoria;
+    categoria.cor       = req.body.cor;
+
+    categoria.save()
+        .then(resultado => res.status(200).json(resultado) )
+        .catch(erro => res.status(500).json(erro) )
+}
+
+module.exports.atualizarCategoria = (app, req, res) => {
+    const id   = req.params.id;
+    const body = req.body;
+
+    categoriaModel.update({ _id: id },body)
+        .then(() => {
+            categoriaModel.findById(id)
+                .then(resultado => res.status(200).json(resultado) );
+        })
+        .catch(error => res.status(500).json(error) )
+}
+
+module.exports.listarTodasCategorias = (app, req, res) => {
     
+    categoriaModel.find()
+        .then((resultado) => {
+            res.status(200).json(resultado);
+        })
+        .catch( error => res.status(500).json(error) )
+
 }
--- app/routes/categorias.js
+++ app/routes/categorias.js
@@ -1,6 +1,16 @@
 module.exports = (app) => {
     app.post('/blog/categorias/cadastrar', (req, res) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
-        app.app.controller.Categoria.categoriaController.cadastroCategoria(app, req, res)
+        app.app.controller.Categoria.categoriaController.cadastroCategoria(app, req, res);
+    });
+
+    app.patch('/blog/categoria/atualizar/:id', (req, res) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        app.app.controller.Categoria.categoriaController.atualizarCategoria(app, req, res);
+    });
+
+    app.get('/blog/categorias/listarCategoria', (req, res) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        app.app.controller.Categoria.categoriaController.listarTodasCategorias(app, req, res);
     })
-}
+};
