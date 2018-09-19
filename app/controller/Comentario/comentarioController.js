9e9b99e40ce42a580adfd3608b0964ff2e3e2b89
rafael_alcindo <rafael_alcindo@hotmail.com>
2018-09-19 15:02:58 -0300

    comentario cadastrar

--- app/controller/Comentario/comentarioController.js
+++ app/controller/Comentario/comentarioController.js
@@ -10,11 +10,12 @@ const especialidadeModel = mongoose.model('Especialidade', BlogEspecialidadeSche
 comentario = new comentarioModel;
 
 module.exports.inserirComentario = (app, req, res) => {
-    let comentario = req.body.comentario;
+    let comentario_body = req.body.comentario;
     let id_usuario = req.body.id_usuario;
     let id_especialidade = req.body.id_especialidade;
 
-    comentario.texto = comentario;
+    comentario._id   = new mongoose.Types.ObjectId;
+    comentario.texto = comentario_body;
 
     usuarioModel.findById(id_usuario)
         .then(result => {
@@ -28,8 +29,37 @@ module.exports.inserirComentario = (app, req, res) => {
                 foto_perfil: result.foto_perfil
             }
 
-            console.log('Usuario: ', usuario);
+            especialidadeModel.findById(id_especialidade)
+                .then(resultEspeci => {
+                    
+                    let especialidade = {
+                        _id: resultEspeci._id,
+                        titulo: resultEspeci.titulo,
+                    }
+
+                    comentario.usuario = usuario;
+                    comentario.especialidade = especialidade
+
+                    comentario.save()
+                        .then(resultado => {
+                            
+                            let comentario = {
+                                texto: resultado.texto,
+                                usuario: resultado.usuario
+                            }
+
+                            especialidadeModel.update({_id: id_especialidade},{$set: { comentarios: [comentario] }})
+                                .then(result => {
+                                    especialidadeModel.findById(id_especialidade)
+                                        .then(resultatdoEspeciUp => res.status(200).json(resultatdoEspeciUp) )
+                                        .catch(error => res.status(500).json(error) )
+                                }).catch(error => res.status(500).json(error) )
+                            
+                        }).catch(error => res.status(500).json(error) )
+                })
+                .catch(error => res.status(500).json(error) )
 
         })
+        .catch(error => res.status(500).json(error) )
 
 }
