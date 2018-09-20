2acd47ab2f335e49969a0dbfdc53e863b1da1305
rafael_alcindo <rafael_alcindo@hotmail.com>
2018-09-20 11:26:13 -0300

    delete comentario

--- app/routes/comentario.js
+++ app/routes/comentario.js
@@ -5,5 +5,8 @@ module.exports = (app) => {
         app.app.controller.Comentario.comentarioController.inserirComentario(app, req, res);
     })
 
-    
+    app.delete('/blog/comentario/deletarComentario', (req, res) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        app.app.controller.Comentario.comentarioController.excluirComentario(app, req, res);
+    })
 }
