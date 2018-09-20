4976544101d27c06b4c3d0cece7dbcf7dccd8a82
rafael_alcindo <rafael_alcindo@hotmail.com>
2018-09-20 14:23:10 -0300

    file upload usuario

--- app/controller/Usuario/usuarioController.js
+++ app/controller/Usuario/usuarioController.js
@@ -3,17 +3,31 @@ const UsuarioSchema = require('../../models/usuarioModel');
 const usuarioModel  = mongoose.model('Desbravadores', UsuarioSchema);
 var usuario = new usuarioModel;
 
+const crypApelido = 'aes-256-ctr';
+// ====== Arquivos de Biblioteca ======
+
+var crypto  = require('crypto');
+var fs      = require('fs');
+
 module.exports.cadastroDesbravadorMongo = (app, req, res) => {
+
     usuario._id         = new mongoose.Types.ObjectId;
     usuario.nome        = req.body.nome;
     usuario.sobrenome   = req.body.sobrenome;
     usuario.login       = req.body.login;
-    usuario.senha       = req.body.senha;
+    usuario.senha       = criptografarArquivo(crypApelido,req.body.senha)  ;
     usuario.privilegio  = 1;
     usuario.descricao   = req.body.descricao;
     usuario.clube       = req.body.clube;
     usuario.unidade     = req.body.unidade;
-    usuario.foto_perfil = req.body.foto_perfil;
+    //usuario.foto_perfil = req.body.foto_perfil;
+
+    verificaExtencaoDoArquivo(req.files.foto_perfil)
+        .then(() => { }).catch(() => res.status(500).json({'error':'arquivo não permitido' }) );
+
+    let resu_foto_perfil = UploadFotoPerfil(req.files.foto_perfil, req.body.login);
+    usuario.foto_perfil = resu_foto_perfil;
+
     //console.log('Desbravador: ', usuario);
     usuario.save()
         .then((user) => res.status(200).json(user) )
@@ -49,6 +63,28 @@ module.exports.atualizarDadosUsuario = (app, req, res) => {
         .catch(err => res.status(500).json(err) )
 }
 
+
+module.exports.atualizarFotoPerfil = (app, req, res) => {
+    const id = req.params.id;
+    
+    verificaExtencaoDoArquivo(req.files.foto_perfil)
+        .then(()=>{ }).catch(() => res.status(500).json({'error':'arquivo não permitido' }) )
+
+    usuarioModel.findById(id)
+        .then(resultado => {
+            let dir = resultado.foto_perfil;
+            fs.unlink(dir);
+            let resu_foto_upload = UploadFotoPerfil(req.files.foto_perfil, req.body.login);
+            usuarioModel.update({_id: id}, { $set: { 'foto_perfil': resu_foto_upload } })
+                .exec().then(resulUp => {
+                    if(resulUp){
+                        usuarioModel.findById(id).then(resu => res.status(200).json(resu))
+                        .catch(error => res.status(500).json(error) )
+                    }
+                }).catch(error => res.status(500).json(error) )
+        }).catch(error => res.status(500).json(error) )
+}
+
 // ============================== Privilégios Administrativo ===============================
 
 
@@ -65,4 +101,59 @@ module.exports.mudarPrivilegios = (app, req, res) => {
                     }).catch(error => { res.status(500).json(error) })
             }
         }).catch(error => res.status(500).json(error) )
+}
+
+
+
+
+
+// ================================= Funções Complementares ================================
+
+// ============= Upload Images ========================
+
+function verificaExtencaoDoArquivo(nomeArquivo){
+    return new Promise((resolve, reject) => {
+        let arrayExtencao = ['.jpg','.png'];
+        let i = nomeArquivo.name.lastIndexOf('.');
+        let extencao = nomeArquivo.name.substr(i);
+        arrayExtencao.filter(filter => {
+            if(filter === extencao){
+                resolve();
+            } 
+        });
+        reject();
+    });
+}
+
+function UploadFotoPerfil(file, email) {
+    let dir = "app/assets/file_users"+criptografarArquivo(crypApelido, email);
+    try {
+        fs.mkdirSync(dir);
+        if(file){
+            file.mv(dir+"/"+file.name);
+            dir = dir+"/"+file.name;
+        } else { throw e }
+        return dir;
+    }catch(e){
+        return null
+    }
+}
+
+
+
+
+// ============================== Criptografar arquivo =========================
+
+function criptografarArquivo(texto, arquivo){
+    let cipher  = crypto.createCipher(texto, arquivo);
+    let crypted = cipher.update(texto,'utf8','hex');
+    crypted += cipher.final('hex');
+    return crypted;
+}
+
+function descriptografarArquivo(texto, arquivo) {
+    let decipher = crypto.createCipher(texto, arquivo);
+    let dec      = cipher.update(texto,'hex','utf8');
+    dec         += decipher.final('utf8');
+    return dec;
 }
